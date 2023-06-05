86381fa3500d3192dfc753842e7731f1c41257b2
Adam-Kasumovic <adam.kasumovic@ghamut.com>
2023-06-05 13:03:56 -0400

    Fixed input index.

--- s2aff/__init__.py
+++ s2aff/__init__.py
@@ -99,10 +99,10 @@ def set_s2aff_vars(ror_index_, look_for_grid_and_isni_, no_candidates_output_tex
 
 
 def reranking_multi(inputs, **kwargs):
-    # inputs[-1][-1] will be the batch size
+    # inputs[-1][-2] will be the batch size
     func = partial(process_item, **kwargs)
     with multiprocessing.get_context("spawn").Pool(multiprocessing.cpu_count()) as pool:
-        generator = pool.imap_unordered(func, inputs, inputs[-1][-1]//multiprocessing.cpu_count())  # TODO: the batch size (third arg) could use some work
+        generator = pool.imap_unordered(func, inputs, inputs[-1][-2]//multiprocessing.cpu_count())  # TODO: the batch size (third arg) could use some work
         for result in generator:
             yield result
 
